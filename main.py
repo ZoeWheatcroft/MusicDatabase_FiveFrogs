83c843a5a551cbac60e3d4c2ca8558b2d52f11d0
Hanna Koh <hjk5643@g.rit.edu>
2022-10-20 22:06:07 -0400

    messing with code again

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from PostgresSSH import getCursor, closeDB
-
-cur = getCursor()
-cur.execute("SELECT * FROM users")
+from PostgresSSH import getMyDB, closeDB
+conn = getMyDB()
+cur = conn.cursor()
+cur.execute("SELECT * FROM album")
 for x in cur:
     print(x)
 
-cur.execute("INSERT INTO album(album_name, release_date, albumID) values (%s, %s, %s)", ( "Midnights", 10020, "10/20/2022" ))
+#cur.execute("INSERT INTO album(album_name, release_date, albumID) values (%s, %s, %s)", ( "Midnights", 10020, "10/20/2022" ))
 
-closeDB()
+closeDB(conn)
