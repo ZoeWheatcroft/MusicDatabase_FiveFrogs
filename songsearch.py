5b3edaa3c9dc4e51e876e258d766c535afc57743
Hanna Koh <hjk5643@g.rit.edu>
2022-10-25 14:53:33 -0400

    working search by name,

--- songsearch.py
+++ songsearch.py
@@ -1,13 +1,20 @@
 import dbaccess
 import useraccess
 
-# need to figure out how to also get user's number of times listened
+# search by name
 def search_name(username): 
-    word = input("What is the name of the song that you're searching for? ")
-    lst = dbaccess.execute_query("SELECT title, length FROM song WHERE title LIKE '%" + word + "%' ORDER BY title ASC" )
-    if len(lst) > 0: 
-        for i in lst: 
-            print(i)
+    quit = False
+    while(not quit):
+        word = input("What is the name of the song that you're searching for? ")
+        lst = dbaccess.execute_query("SELECT s.title, s.length, u.play_history FROM song AS s LEFT OUTER JOIN userplayssong AS u ON ('s.songID' = 'u.songID') WHERE title LIKE '%" + word + "%' ORDER BY title ASC" )
+        print(len(lst), "Songs found!")
+        if len(lst) > 0: 
+            for i in lst: 
+                print(i)
+        c = input("Would you like to search again? ")
+        if c.upper()[0] == "N": 
+            quit = True
+    return None
 
 if __name__ == '__main__': 
     username = useraccess.login()
