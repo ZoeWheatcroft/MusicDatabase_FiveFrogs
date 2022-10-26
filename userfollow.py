9a447f731d822b4f532ad3e94ee5a0238373622c
Sam Ellis <sae1200@rit.edu>
2022-10-26 17:40:17 -0400

    added follow_user

--- userfollow.py
+++ userfollow.py
@@ -5,8 +5,18 @@ import usersearch
 
 # follow a user 
 def follow_user(username):
-    return 0
-
+    quit = False
+    lst = None
+    while(not quit):
+        word = input("What is the email of the user that you would like to follow? ")
+        # TODO: SQL NEEDS TO CHECK IF THE USER IS ALREADY BEING FOLLOWED
+        lst = dbaccess.execute_query("UPDATE userfollowsuser \
+            SET follows = CONCAT('"+username+"', ', ', follows) \
+            WHERE username = '"+username+"'")
+        c = input("Would you like to follow another user? ")
+        if c.upper()[0] == 'N':
+            quit = True
+    return lst 
 
 if __name__ == '__main__':
     username = useraccess.login()
