574b11b3ce8a65eed3a2c5c842bb9c7052490744
Hanna Koh <hjk5643@g.rit.edu>
2022-10-26 16:24:46 -0400

    can play a song by name!

--- playsong.py
+++ playsong.py
@@ -3,13 +3,14 @@ import useraccess
 import songsearch
 
 def play_songID(s_id, user):
-    lst = dbaccess.execute_query("SELECT title, length, song_id, listen_count from SONG where song_id = '%s'" % (s_id))
+    lst = dbaccess.execute_query("SELECT title, length,  song_id, listen_count from SONG where song_id = '%s'" % (s_id))
     print("Playing song", lst[0][0], "for", lst[0][1], "seconds.")
     dbaccess.execute_start("UPDATE song SET listen_count = '%d' WHERE song_id = %s" %  (int(lst[0][3])+1, lst[0][2]))
     lst = dbaccess.execute_query("SELECT * FROM userplayssong WHERE username = '%s' AND song_id = '%s'" % (user, s_id))
     if len(lst) == 0: 
-        dbaccess.execute_start("INSERT INTO userplayssong VALUES('%s', '%s', '0')")
-    dbaccess.execute_start("UPDATE userplayssong SET play_history = '%d' WHERE song_id = %s AND username = '%s'" %  (int(lst[0][3])+1, lst[0][2], user))
+        dbaccess.execute_start("INSERT INTO userplayssong VALUES('%s', '%s', '0')" % (user, s_id))
+    lst = dbaccess.execute_query("SELECT * FROM userplayssong WHERE username = '%s' AND song_id = '%s'" % (user, s_id))
+    dbaccess.execute_start("UPDATE userplayssong SET play_history = '%d' WHERE song_id = %s AND username = '%s'" %  (int(lst[0][2])+1, lst[0][1], user))
 
 def play_song(user): 
     lst = []
