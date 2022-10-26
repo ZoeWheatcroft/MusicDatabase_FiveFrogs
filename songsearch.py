c0bc5a8cff424ff6433477755575fd0bcb4eaf50
Hanna Koh <hjk5643@g.rit.edu>
2022-10-25 20:33:32 -0400

    finished search by name

--- songsearch.py
+++ songsearch.py
@@ -6,16 +6,16 @@ def search_name(username):
     quit = False
     while(not quit):
         word = input("What is the name of the song that you're searching for? ")
-        lst = dbaccess.execute_query("SELECT s.title, s.length, u.play_history FROM song AS s LEFT OUTER JOIN userplayssong AS u ON ('s.songID' = 'u.songID') WHERE title LIKE '%" + word + "%' ORDER BY title ASC" )
+        lst = dbaccess.execute_query("SELECT title, length, listen_count from song WHERE title LIKE '%" + word + "%' ORDER BY title ASC" )
         print(len(lst), "Songs found!")
         if len(lst) > 0: 
             for i in lst: 
-                print(i)
+                print("Song Title: %18s | Length (sec): %2d | Number of plays: %3d" % (i[0], i[1], i[2]))
         c = input("Would you like to search again? ")
         if c.upper()[0] == "N": 
             quit = True
     return None
 
 if __name__ == '__main__': 
-    username = useraccess.login()
-    search_name(username)
+    #username = useraccess.login()
+    search_name("hi")
