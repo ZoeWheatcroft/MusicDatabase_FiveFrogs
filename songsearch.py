f7e39ef8efdc17497ef89195d8ac5e22aa49c7fe
Hanna Koh <hjk5643@g.rit.edu>
2022-10-27 20:43:24 -0400

    fixed search

--- songsearch.py
+++ songsearch.py
@@ -9,9 +9,9 @@ def search_name():
         word = input("What is the name of the song that you're searching for? ")
         lst = dbaccess.execute_query("SELECT a.artist_name, s.title, s.length, s.listen_count, k.album_name \
                 FROM song AS s \
-                LEFT JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
+                INNER JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
                 LEFT JOIN artistcreatesalbum AS t ON (a.artist_name = t.artist_name) \
-                LEFT JOIN album as k ON (t.album_id = k.album_id) \
+                INNER JOIN album as k ON (t.album_id = k.album_id) \
                 WHERE s.title LIKE '%"+word+"%' AND s.song_id = a.song_id \
                 ORDER BY s.title, a.artist_name ASC")
         print(len(lst), "Songs found!")
@@ -19,7 +19,7 @@ def search_name():
             for i in lst: 
                 print("Artist Name: %16s | Song Title: %18s | Length (sec): %2d | Number of plays: %3d | Album Name: %10s " % (i[0], i[1], i[2], i[3], i[4]))
        
-        if(u.keep_asking("Would you like to search again?")):
+        if(u.keep_asking("Would you like to search by name again?")):
             quit = True
     return lst
 
@@ -31,16 +31,16 @@ def search_artist():
         word = input("Who created the song that you're searching for? ")
         lst = dbaccess.execute_query("SELECT a.artist_name, s.title, s.length, s.listen_count, k.album_name \
                 FROM song AS s \
-                LEFT JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
+                INNER JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
                 LEFT JOIN artistcreatesalbum AS t ON (a.artist_name = t.artist_name) \
-                LEFT JOIN album as k ON (t.album_id = k.album_id) \
+                INNER JOIN album as k ON (t.album_id = k.album_id) \
                 WHERE a.artist_name = '"+ word +"' AND s.song_id = a.song_id \
                 ORDER BY s.title, a.artist_name ASC")
         print(len(lst), "Songs found!")
         if len(lst) > 0: 
             for i in lst: 
                 print("Artist Name: %10s | Song Title: %18s | Length (sec): %2d | Number of plays: %3d | Album Name: %10s " % (i[0], i[1], i[2], i[3], i[4]))
-        if(u.keep_asking("Would you like to search again?")):
+        if(u.keep_asking("Would you like to search by artist again?")):
             quit = True
     return lst
 
@@ -52,16 +52,16 @@ def search_album():
         word = input("Which album is the song you're searching for a part of? ")
         lst = dbaccess.execute_query("SELECT a.artist_name, s.title, s.length, s.listen_count, k.album_name \
                 FROM song AS s \
-                LEFT JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
+                INNER JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
                 LEFT JOIN artistcreatesalbum AS t ON (a.artist_name = t.artist_name) \
-                LEFT JOIN album as k ON (t.album_id = k.album_id) \
+                INNER JOIN album as k ON (t.album_id = k.album_id) \
                 WHERE k.album_name = '"+word+"' AND s.song_id = a.song_id \
                 ORDER BY s.title, a.artist_name ASC")
         print(len(lst), "Songs found!")
         if len(lst) > 0: 
             for i in lst: 
                 print("Artist Name: %14s | Song Title: %18s | Length (sec): %2d | Number of plays: %3d | Album Name: %10s " % (i[0], i[1], i[2], i[3], i[4]))
-        if(u.keep_asking("Would you like to search again?")):
+        if(u.keep_asking("Would you like to search by album again?")):
             quit = True
     return lst
 
@@ -73,17 +73,17 @@ def search_genre():
         word = input("Which genre is the song that you're searching for from? ")
         lst = dbaccess.execute_query("SELECT g.genre_name, a.artist_name, s.title, s.length, s.listen_count, k.album_name \
                 FROM song AS s \
-                LEFT JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
+                INNER JOIN artistcreatessong AS a ON(s.song_id = a.song_id) \
                 LEFT JOIN artistcreatesalbum AS t ON (a.artist_name = t.artist_name) \
-                LEFT JOIN album as k ON (t.album_id = k.album_id) \
-                LEFT JOIN songhasgenre as g ON (g.song_id = s.song_id) \
+                INNER JOIN album as k ON (t.album_id = k.album_id) \
+                INNER JOIN songhasgenre as g ON (g.song_id = s.song_id) \
                 WHERE g.genre_name = '"+word+"' AND s.song_id = a.song_id \
                 ORDER BY s.title, a.artist_name ASC")
         print(len(lst), "Songs found!")
         if len(lst) > 0: 
             for i in lst: 
                 print("Genre Name: %6s | Artist Name: %14s | Song Title: %18s | Length (sec): %2d | Number of plays: %3d | Album Name: %10s " % (i[0], i[1], i[2], i[3], i[4], i[5]))
-        if(u.keep_asking("Would you like to search again?")):
+        if(u.keep_asking("Would you like to search by genre again?")):
             quit = True
     return lst
 
@@ -96,24 +96,31 @@ def search_screen():
     print("  4. By genre")
     print("  5. Exit")
     num = input("Enter your selection here: [1, 2, 3, 4, 5] ")
-    valid = False
-    while not valid:
-        if num == "1": 
-            valid = True
-            search_name()
-        elif num == "2": 
-            valid = True
-            search_artist()
-        elif num == "3": 
-            valid = True
-            search_album()
-        elif num == "4": 
-            valid = True
-            search_genre()
-        elif num == "5": 
-            break
-        else: 
-            num = input("Incorrect value. Please try again: [1, 2, 3, 4, 5] ")
+    quit = False
+    while not quit:
+        valid = False
+        while not valid:
+            if num == "1": 
+                valid = True
+                search_name()
+            elif num == "2": 
+                valid = True
+                search_artist()
+            elif num == "3": 
+                valid = True
+                search_album()
+            elif num == "4": 
+                valid = True
+                search_genre()
+            elif num == "5": 
+                quit = True
+                break
+            else: 
+                num = input("Incorrect value. Please try again: [1, 2, 3, 4, 5] ")
+        yn = input("Search by another category? [Y/N] ")
+        if yn.lower()[0] == "n": 
+            quit = True
+
 
 
 if __name__ == '__main__': 
