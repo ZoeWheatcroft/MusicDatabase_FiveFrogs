32fe4ab28690ddb555244f575cc4534571343161
Sam Ellis <sae1200@rit.edu>
2022-11-05 15:06:13 -0400

    created top 50 file and stubbed functions

---
+++ top50.py
@@ -0,0 +1,13 @@
+import dbaccess
+
+# Returns the top 50 most popular songs in the last 30 days (rolling)
+def find_top50_recent():
+    return
+
+
+# Returns the top 50 most popular songs among my friends
+def find_top50_friends():
+    return
+
+if __name__ == '__main__':
+    print("I like frogs")
