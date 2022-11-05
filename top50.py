4b19a84af681c33324490f48ecc3a2b6552e3cb5
Sam Ellis <sae1200@rit.edu>
2022-11-05 15:18:55 -0400

    added select query and print header

--- top50.py
+++ top50.py
@@ -1,13 +1,25 @@
 import dbaccess
+from userfollow import print_list
+
+def top50_header(dash_len):
+    list_name = "~*~ Top 50 Charts ~*~"
+    print_list(list_name, dash_len)
+    line = "-" * dash_len
+    print("%18s | %18s | %10s | %10s" % ("Rank","Artist Name", "Song Title", "Album"))
+    print(line)
 
 # Returns the top 50 most popular songs in the last 30 days (rolling)
 def find_top50_recent():
-    return
-
+    lst = dbaccess.execute_query("SELECT s.title \
+                                FROM song AS s \
+                                ORDER BY s.listen_count DESC \
+                                LIMIT 50")
+    dash_len = 70
+    top50_header(dash_len)
 
 # Returns the top 50 most popular songs among my friends
 def find_top50_friends():
     return
 
 if __name__ == '__main__':
-    print("I like frogs")
+    find_top50_recent()
