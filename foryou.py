48c85f7bddc487577c666c144ed81f30af5c0e7d
Hanna Koh <hjk5643@g.rit.edu>
2022-11-11 20:10:26 -0500

    GENRE RECCOMENDATION WORKS!!

--- foryou.py
+++ foryou.py
@@ -23,6 +23,31 @@ def fyp(username):
     WHERE p.song_id IS null \
     AND fp.user_id = '%s' \
     ORDER BY fp.user_id, fp.like_count DESC" % (username))
+    # if no friends, look at play history & songs in genre
+    if len(lst) == 0: 
+        lst = d.execute_query("WITH userplaysgenre AS (\
+        SELECT u.username AS users, g.genre_name AS genre FROM userplayssong u \
+        INNER JOIN songhasgenre AS g on u.song_id = g.song_id \
+        ), all_songs AS ( \
+        SELECT users AS users, genre AS genre FROM userplaysgenre \
+        UNION \
+        SELECT genre AS users, user AS genre FROM userplaysgenre \
+        ), genre_has AS ( \
+        SELECT au.users, p.song_id, count(p.genre_name) AS like_count \
+        FROM all_songs AS au \
+        LEFT JOIN songhasgenre p ON au.genre = p.genre_name \
+        GROUP BY au.users, p.song_id \
+        ) \
+         SELECT s.title, a.artist_name, s.length \
+    FROM genre_has AS gh \
+    LEFT JOIN userplayssong AS p ON gh.users = p.username AND gh.song_id = p.song_id \
+    INNER JOIN song AS s ON s.song_id = gh.song_id \
+    INNER JOIN artistcreatessong AS a ON s.song_id = a.song_id\
+    WHERE p.song_id IS null \
+    AND gh.users = '%s' \
+    ORDER BY gh.users, gh.like_count DESC" % (username))
+    if( len(lst) == 0 ): 
+        print("No play history found! Go play some songs!")
     for i in lst: 
         duration = convert_mins(i[2])
         print("Song Title: %18s | Artist Name: %14s | Length (sec): %2s" % (i[0], i[1], duration))
